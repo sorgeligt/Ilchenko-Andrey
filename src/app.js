539cbc4d26202a56278fde51145f1f24a617f375
sorgeligt <ubutno@gmail.com>
2021-11-15 02:57:32 +0300

    gulp optimize image path in css

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('checkbox_theme').onclick = function () {
+function changeThemeListener() {
+    const button = document.getElementById('checkbox_theme');
+    button.addEventListener('click', () => {
         document.querySelector("#body").classList.toggle("light-mode");
-    }
-})
+    });
+}
+
+document.addEventListener("DOMContentLoaded", changeThemeListener);
