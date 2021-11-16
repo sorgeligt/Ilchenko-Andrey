40d7d4e5839caeed480de655aaadf62a9aecc187
sorgeligt <ubutno@gmail.com>
2021-11-16 16:07:33 +0300

    button update

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,20 @@ function changeThemeListener() {
 
 document.addEventListener("DOMContentLoaded", changeThemeListener);
 
-const input = document.getElementById("search-input");
+//const input1 = document.getElementById("search-input1");
+const input2 = document.getElementById("search-input2");
+const input3 = document.getElementById("search-input3");
 const searchBtn = document.getElementById("search-btn");
 
 const expand = () => {
     searchBtn.classList.toggle("close");
     searchBtn.classList.toggle("plus");
-    input.classList.toggle("square");
+        // input1.classList.toggle("square");
+        // input1.classList.toggle("hidden");
+    input2.classList.toggle("square");
+    input2.classList.toggle("hidden");
+    input3.classList.toggle("square");
+    input3.classList.toggle("hidden");
 };
 
 searchBtn.addEventListener("click", expand);
